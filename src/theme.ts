385087a4ab62f4e508b85e658ec56b10f2e3a923
Vactor0911 <vactor0911@gmail.com>
2024-09-07 20:17:35 +0900

    Added theme color

--- src/theme.ts
+++ src/theme.ts
@@ -3,4 +3,8 @@ export const color = {
   gray: "#C0C0C0",
   black: "#252525",
   red: "#FF2222",
+  stratagem_blue: "#5dbcd6",
+  stratagem_yellow: "#c9b269",
+  stratagem_green: "#679552",
+  stratagem_red: "#de7b6c",
 };
